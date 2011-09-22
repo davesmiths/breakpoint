7144fc0cbf4f0056f83f6dfd7299626a02bbb535
Dave Smith <da__smith@hotmail.com>
2011-09-22 17:24:16 +0000

    Code now even smaller

--- breakpoint.src.js
+++ breakpoint.src.js
@@ -2,7 +2,7 @@
 
     var breakpoint = window['https://github.com/davesmith/breakpoint'] = {
         
-        widths: [320, 480, 600, 720, 768, 800, 1024, 1080, 1280, 1440],
+        breakpoints: [320, 480, 600, 720, 768, 800, 1024, 1080, 1280, 1440],
         // Breakpoints: http://www.slideshare.net/yiibu/pragmatic-responsive-design
         // 320andup 320, 480, 768, 992 and 1382 (find the reasoning behind 992 and 1382)
         // HD 720, 1080, 1440
@@ -13,66 +13,71 @@
             var //breakpoint = win['https://github.com/davesmith/breakpoint'] || {},
                 browserWidth = win.innerWidth || docElement.clientWidth || doc.body.clientWidth || 0,
                 i = 0,
-                widths = breakpoint.widths,
-                widthsLength = widths.length,
-                width,
+                j = 0,
+                breakpoints = breakpoint.breakpoints,
+                breakpointsLength = breakpoints.length,
+                breakpoint_i,
+                breakpoint_j,
                 docElementClass = '',
                 ltePrefix = 'lte',
-                gtPrefix = 'gt';
+                gtPrefix = 'gt',
+                gt;
                 
-            lte = [];
-            for (i; i < widthsLength; i++) {
-                width = widths[i];
-                if (browserWidth <= width) {
-                    lte.push(width);
-                    docElementClass += ' ' + ltePrefix + width;
+            for (i = 0; i < breakpointsLength; i++) {
+                breakpoint_i = breakpoints[i];
+                gt = 0;
+                if (browserWidth > breakpoint_i) {
+                    docElementClass += ' ' + gtPrefix + breakpoint_i;
+                    gt = 1;
                 }
-            }
-            lteLength = lte.length;
-            for (i = 0; i < widthsLength; i++) {
-                width = widths[i];
-                if (browserWidth > width) {
-                    docElementClass += ' ' + gtPrefix + width;
-                    for (j = 0; j < lteLength; j++) {
-                        docElementClass += ' ' + gtPrefix + width + ltePrefix + lte[j];
+                for (j = 0; j < breakpointsLength; j++) {
+                    breakpoint_j = breakpoints[j];
+                    if (browserWidth <= breakpoint_j) {
+                        docElementClass += ' ' + ltePrefix + breakpoint_j;
+                        if (gt === 1) {
+                            docElementClass += ' ' + gtPrefix + breakpoint_i + ltePrefix + breakpoint_j;
+                        }
                     }
                 }
             }
-            docElement.className = docElement.className.replace(new RegExp(" " + breakpointString + ".*" + breakpointString + " ", 'g'), ' ' + breakpointString + ' ' + docElementClass + ' ' + breakpointString + ' ');
+            docElement[className] = docElement[className].replace(breakpointRegExp, ' ' + breakpointString + ' ' + docElementClass + ' ' + breakpointString + ' ');
         }
     },
     docElement = doc.documentElement,
-    breakpointString = 'breakpoint';
+    className = 'className',
+    breakpointString = 'Br34kp01nT',
+    breakpointRegExp = new RegExp(" " + breakpointString + ".*" + breakpointString + " ", 'g');
 
     // Seed the HTML element class attribute value with a placeholder to be replaced.
-    docElement.className += ' ' + breakpointString + ' ' + breakpointString + ' ';
+    docElement[className] += ' ' + breakpointString + ' ' + breakpointString + ' ';
     
     // Fire once.
     breakpoint.update();
     
     // Handle the resize event.
-    var winRemoveEventListener = win.removeEventListener || false,
-    winAddEventListener = win.addEventListener || false,
-    winAttachEvent = win.attachEvent || false,
-    winDetachEvent = win.detachEvent || false,
+    var fls = false,
+    winRemoveEventListener = win.removeEventListener || fls,
+    winAddEventListener = win.addEventListener || fls,
+    winAttachEvent = win.attachEvent || fls,
+    winDetachEvent = win.detachEvent || fls,
     resizeString = 'resize';
-	function rzr() {
-		if (winRemoveEventListener) {
-			winRemoveEventListener(resizeString, rzr, false);
+    function rzr() {
+    	if (winRemoveEventListener) {
+			winRemoveEventListener(resizeString, rzr, fls);
 		}
 		else if (winDetachEvent) {
 			winDetachEvent('on' + resizeString, rzr);
 		}
 		breakpoint.update();
 		if (winAddEventListener) {
-			winAddEventListener(resizeString, rzr, false);
+			winAddEventListener(resizeString, rzr, fls);
 		}
 		else if (winAttachEvent) {
 			winAttachEvent('on' + resizeString, rzr);
 		}
 	}
 	if (winAddEventListener) {
-		winAddEventListener(resizeString, rzr, false);
+		winAddEventListener(resizeString, rzr, fls);
 	}
 	else if (winAttachEvent) {
 		winAttachEvent('on' + resizeString, rzr);
